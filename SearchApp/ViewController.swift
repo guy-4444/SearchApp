4f37a843ecb2306bbb2b9fd8de25d5133db7b3a0
Guy <guyskv@gmail.com>
2020-05-04 22:38:08 +0300

    fix text field functions

--- SearchApp/ViewController.swift
+++ SearchApp/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var main_EDT_search: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        main_EDT_search.delegate = self
     }
 
+    @IBAction func searchClicked(_ sender: Any) {
+        var input: String = main_EDT_search.text ?? ""
+        print(input)
+    }
+    
 
+    
 }
+
+extension ViewController: UITextFieldDelegate {
+    
+        func search(input: String) {
+            main_EDT_search.endEditing(true)
+            print("\(input)")
+        }
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            var input = main_EDT_search.text ?? ""
+            search(input: input)
+            return true
+        }
+        
+        func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+            if main_EDT_search.text != "" {
+                return true
+            } else {
+                print("try again")
+                return false
+            }
+            
+        }
+        
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            main_EDT_search.text = ""
+        }
+        
+    }
 
